d4b54c6de6d48917f4fc45fa4ba9df0343d7c762
Aleksandra Petkova <a.petkova1617@gmail.com>
2020-08-18 20:59:10 +0100

    Update JointDetection.py

--- JointDetection.py
+++ JointDetection.py
@@ -149,12 +149,3 @@ if __name__ == "__main__":
     k.create_model({"input": [], "output": []}, {"input": [], "output": []})
     k.predict([])
 
-    #-----------------------------------------------------------------
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    #objp = np.zeros((6 * 7, 3), np.float32)
-    #objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
-
-    # Arrays to store object points and image points from all the images.
-    #objpoints = []  # 3d point in real world space
-    #imgpoints = []  # 2d points in image plane.
-
